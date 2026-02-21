f3f9561a621e5c2494e59b45101c29002e47a12d
anamade <50694980+anamade@users.noreply.github.com>
2026-02-21 11:14:53 +0700

    feat: enhance Result page with project data fetching and display, add pull request template

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,5 +1,68 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import type { Project } from '../types';
+import { dummyGenerations } from '../assets/assets';
+import { Loader2Icon, RefreshCwIcon } from 'lucide-react';
+
 const Result = () => {
-  return <div>Result</div>;
+  const [project, setProjectData] = useState<Project>({} as Project);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProjectData = () => {
+    const timer = setTimeout(() => {
+      setProjectData(dummyGenerations[0]);
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    const cleanup = fetchProjectData();
+    return cleanup;
+  }, []);
+
+  return loading ? (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Loader2Icon className="animate-spin text-indigo-500 size-9" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 mt-20">
+      <div className="max-w-6xl mt-auto">
+        <header className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl md:text-3xl font-medium">Generation Result</h1>
+          <Link to="/generate" className="btn-secondary text-sm flex items-center gap-2">
+            <RefreshCwIcon className="w-4 h-4" />
+            <p className="max-sm:hidden">New Generation</p>
+          </Link>
+        </header>
+
+        {/* {grid layout} */}
+        <div className="grid lg:grid-cols-3 gap-8">
+          {/* {main result display} */}
+          <div className="lg:col-span-2 space-y-6">
+            <div className="glass-panel inline-block p-2 rounded-2xl">
+              <div
+                className={`${project?.aspectRatio === '9:16' ? 'aspect-9/16' : 'aspect-video'}
+              sm:max-h-200 rounded-xl bg-gray-900 overflow-hidden relative`}
+              >
+                {project.generatedVideo ? (
+                  <video src={project.generatedVideo} controls autoPlay loop className="w-full h-full object-cover" />
+                ) : (
+                  <img src={project.generatedImage} alt="Generated Result" className="w-full h-full object-cover" />
+                )}
+              </div>
+            </div>
+          </div>
+          {/* {Sidebar Actions} */}
+          <div className="space-y-6">
+            {/* {Download Button} */}
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Result;
